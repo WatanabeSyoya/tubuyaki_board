68e8b81661882554013ac09bec91ade90ee5bf0d
WatanabeSyoya <76427150+WatanabeSyoya@users.noreply.github.com>
2021-02-28 18:43:27 +0900

    カテゴリ一覧

--- src/app/Http/Controllers/PostController.php
+++ src/app/Http/Controllers/PostController.php
@@ -15,13 +15,25 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::all();
-        $posts->load('category', 'user' );
-        
+        $q = \Request::query();
 
-        return view('posts.index', [
+        if(isset($q['category_id'])){
+            $posts = Post::latest()->where('category_id', $q['category_id'])->get();
+            $posts->load('category', 'user');
+
+            return view('posts.index', [
+                'posts' => $posts,
+                'category_id' => $q['category_id']
+            ]);
+
+        } else {
+            $posts = Post::latest()->get();
+            $posts->load('category', 'user');
+
+            return view('posts.index', [
             'posts' => $posts,
-        ]);
+            ]);
+        }
     }
 
     /**
