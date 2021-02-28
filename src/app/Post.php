9a84a226426ac5b45ee673727b33eb3f8166c819
WatanabeSyoya <76427150+WatanabeSyoya@users.noreply.github.com>
2021-02-28 11:11:23 +0900

    投稿一覧画面

--- src/app/Post.php
+++ src/app/Post.php
@@ -10,4 +10,15 @@ class Post extends Model
     protected $fillable = [
         'user_id', 'category_id', 'content', 'title'
     ];
+
+    public function category(){
+        // 投稿は1つのカテゴリーに属する
+        return $this->belongsTo(\App\Category::class,'category_id');
+      }
+
+      public function user(){
+        // 投稿は1つのカテゴリーに属する
+        return $this->belongsTo(\App\User::class,'user_id');
+      }
+
 }
